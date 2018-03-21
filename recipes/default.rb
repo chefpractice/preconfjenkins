6bbddb7da5663414457e13d843dd93e96326a449
Kanakamedala <pkanakam@ms.ds.uhc.com>
2018-03-22 00:02:54 +0530

    jenkins image built , container instance created .

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,49 +14,28 @@ docker_service_manager 'default' do
   action :start
 end
 
-directory '/root/workspace/jenkinsdocker' do
+# directory '/root/workspace/jenkinsdocker' do
+#
+# end
 
+# ['Dockerfile', 'tini_pub.gpg','init.groovy', 'jenkins-support','jenkins.sh', 'tini-shim.sh','plugins.sh','install-plugins.sh'].each do |file|
+#   cookbook_file "/root/workspace/jenkinsdocker/#{file}" do
+#     source "jenkinsdocker/#{file}"
+#     mode "0755"
+#   end
+# end
+
+git '/root/workspace/jenkinsdocker' do
+  repository 'https://github.com/chefpractice/jenkinsdocker.git'
+  revision 'master'
+  action :sync
 end
 
-['Dockerfile', 'tini_pub.gpg','init.groovy', 'jenkins-support','jenkins.sh', 'tini-shim.sh','plugins.sh','install-plugins.sh'].each do |file|
-  cookbook_file "/root/workspace/jenkinsdocker/#{file}" do
-    source "jenkinsdocker/#{file}"
-    mode "0644"
-  end
-end
+  # remote_directory "/root/workspace/jenkinsdocker" do
+  #   source "jenkinsdocker"
+  # end
 
 
-# cookbook_file '/root/workspace/Dockerfile' do
-#   source 'jenkinsdocker/Dockerfile'
-# end
-#
-# cookbook_file '/root/workspace/tini_pub.gpg' do
-#   source 'jenkinsdocker/tini_pub.gpg'
-# end
-#
-# cookbook_file '/root/workspace/init.groovy' do
-#   source 'jenkinsdocker/init.groovy'
-# end
-#
-# cookbook_file '/root/workspace/jenkins-support' do
-#   source 'jenkinsdocker/jenkins-support'
-# end
-#
-# cookbook_file '/root/workspace/jenkins.sh' do
-#   source 'jenkinsdocker/jenkins.sh'
-# end
-#
-# cookbook_file '/root/workspace/tini-shim.sh' do
-#   source 'jenkinsdocker/tini-shim.sh'
-# end
-#
-# cookbook_file '/root/workspace/plugins.sh' do
-#   source 'jenkinsdocker/plugins.sh'
-# end
-#
-# cookbook_file '/root/workspace/install-plugins.sh' do
-#   source 'jenkinsdocker/install-plugins.sh'
-# end
 
 docker_image 'centos_jenkins' do
   tag 'latest'
@@ -64,21 +43,23 @@ docker_image 'centos_jenkins' do
   action :build_if_missing
 end
 
-# docker_container 'masterjenkins' do
-#   repo 'centos_jenkins'
-#   tag 'latest'
-#   #command 'ls -la /'
-#   port ['8080:8080','50000:50000']
-#   #port '50000:50000'
-#   volumes 'jenkins_home:/var/jenkins_home'
-#   action :run
-# end
-
-git '/root/workspace/docker' do
-  repository 'https://github.com/jenkinsci/docker.git'
-  revision 'master'
-  action :sync
+docker_container 'masterjenkins' do
+  repo 'centos_jenkins'
+  tag 'latest'
+  #command 'ls -la /'
+  port ['8080:8080','50000:50000']
+  #port '50000:50000'
+  env ['JENKINS_JAVA_OPTIONS="-Djenkins.install.runSetupWizard=false"', 'JAVA_OPTS="-Xmx2048m -XX:MaxPermSize=512m"']
+  volumes 'jenkins_home:/var/jenkins_home'
+  action :run
 end
+
+
+# git '/root/workspace/docker' do
+#   repository 'https://github.com/jenkinsci/docker.git'
+#   revision 'master'
+#   action :sync
+# end
 
 
 # docker_container 'masterjenkins' do
